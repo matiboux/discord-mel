652aedbd5c457308f6d2299494ae056b27b5f027
Matiboux <matiboux@gmail.com>
2021-11-13 23:43:03 +0100

    Fix type of Services

--- src/Services.ts
+++ src/Services.ts
@@ -1,10 +1,13 @@
 import Config from './Config'
 import DefaultState from './state/DefaultState'
 
-const Services =
+const Services:
+	{
+		Config: typeof Config,
+		State: any,
+	} =
 	{
 		Config: Config,
-
 		State: DefaultState,
 	}
 
