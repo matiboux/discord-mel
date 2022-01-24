32b627cdbe4fb8088a2ef6c47f3077d60232cb6b
Matiboux <matiboux@gmail.com>
2022-01-24 03:24:50 +0100

    Fix Service type constructor

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -2,7 +2,7 @@ import Mel from '../Mel'
 import AbstractService from './AbstractService'
 
 type Service =
-	(new (bot: Mel) => AbstractService) &
+	(new (name: string, bot?: Mel) => AbstractService) &
 	{ [K in keyof typeof AbstractService]: typeof AbstractService[K] }
 
 export default Service
