ab43415db18501f9ddce3323fe6c61e5bbcac008
Matiboux <matiboux@gmail.com>
2022-01-24 03:21:05 +0100

    Add Service type

---
+++ src/services/Service.ts
@@ -0,0 +1,9 @@
+import { Mel } from '..'
+import AbstractService from './AbstractService'
+
+type Service =
+	(new (bot: Mel) => AbstractService) &
+	{ [K in keyof typeof AbstractService]: typeof AbstractService[K] }
+
+
+export default Service
