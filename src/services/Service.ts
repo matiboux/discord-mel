2e10540e91dde5e319edca40103135e255184c9c
Matiboux <matiboux@gmail.com>
2022-01-24 03:23:04 +0100

    Refactor Service type

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,9 +1,8 @@
-import { Mel } from '..'
+import Mel from '../Mel'
 import AbstractService from './AbstractService'
 
 type Service =
 	(new (bot: Mel) => AbstractService) &
 	{ [K in keyof typeof AbstractService]: typeof AbstractService[K] }
-
 
 export default Service
