6ffa488dea48b3c31259c84f548cea740d6b30f8
Matiboux <matiboux@gmail.com>
2022-02-04 23:02:14 +0100

    Fix abstract context type

--- src/config/types/AbstractContext.ts
+++ src/config/types/AbstractContext.ts
@@ -6,27 +6,57 @@ abstract class AbstractContext extends AbstractConfigType implements IContext
 	/**
 	 * Whether or not the bot is enabled in this context.
 	 */
-	public enabled: boolean = true
+	public enabled!: boolean
 
 	/**
 	 * Whether or not the bot should ignore commands from other bots.
 	 */
-	 public ignoreBots: boolean = true
+	 public ignoreBots!: boolean
 
 	/**
 	 * Whether or not the bot should respond to message commands.
 	 */
-	public messageCommands: boolean = false
+	public messageCommands!: boolean
 
 	/**
 	 * The prefix for message commands.
 	 */
-	public prefix: string = '!'
+	public prefix!: string
 
 	/**
 	 * Whether or not the bot should respond to mentions commands.
 	 */
-	public mentionCommands: boolean = false
+	public mentionCommands!: boolean
+
+	public constructor(type?: AbstractConfigType)
+	{
+		super(type)
+
+		if (this.enabled as any === undefined)
+		{
+			this.enabled = true
+		}
+
+		if (this.ignoreBots as any === undefined)
+		{
+			this.ignoreBots = true
+		}
+
+		if (this.messageCommands as any === undefined)
+		{
+			this.messageCommands = true
+		}
+
+		if (this.prefix as any === undefined)
+		{
+			this.prefix = '!'
+		}
+
+		if (this.mentionCommands as any === undefined)
+		{
+			this.mentionCommands = false
+		}
+	}
 }
 
 export default AbstractContext
